41b02ee7512eb6d81051be7921006e5515f1702d
Jacob Eichler <jeichler7@gmail.com>
2022-09-27 18:42:30 -0600

    connected multiple components across app, added animation. Final commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React, { Fragment} from "react";
+import React, {useState} from "react";
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
 import Cart from "./Components/Cart/Cart";
+import CartProvider from "./Store/CartProvider";
 
 
 function App() {
+
+const [cartIsShown, setCartIsShown] = useState(false)
+
+const showCartHandler = () => {
+  setCartIsShown(true)
+}
+
+const hideCartHandler = () => {
+  setCartIsShown(false)
+}
+
   return (
-    <Fragment>
-      <Cart/>
-      <Header />
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler}/>
       <main>
         <Meals />
       </main>
-   </Fragment>
+   </CartProvider>
   );
 }
 
